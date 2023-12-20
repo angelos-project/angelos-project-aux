6a74565e98be927c01d90323fbafdffb9c6de277
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2023-12-20 20:29:04 +0100

    Initial UNIX epoch support

---
+++ src/commonMain/kotlin/org.angproj.aux.util/Aux.kt
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2023 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.angproj.aux.util
+
+import kotlin.math.absoluteValue
+
+public fun Int.floorMod(other: Int): Int = this.absoluteValue.mod(other)
+public fun Long.floorMod(other: Long): Long = this.absoluteValue.mod(other)
